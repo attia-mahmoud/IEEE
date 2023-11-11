a3dda63420e53a9406e97ed40e3b4ce73ae16c8f
Mahmoud Attia <64820897+attia-mahmoud@users.noreply.github.com>
2023-11-11 20:32:56 +0300

    audit: update hero text

--- pages/members.tsx
+++ pages/members.tsx
@@ -1,11 +1,23 @@
 import { Footer } from '@/components/Footer/Footer';
 import { Hero } from '../components/Hero/Hero';
 import { Members } from '@/components/Members/Members';
+import { Text } from '@mantine/core';
 
 export default function MembersPage() {
+  const HeroProps = {
+    title: (
+      <>
+        The Minds Behind the{' '}
+        <Text component="span" inherit variant="gradient" gradient={{ from: 'blue', to: 'cyan' }}>
+          IEEE Student Branch
+        </Text>{' '}
+      </>
+    ),
+  };
+
   return (
     <div>
-      <Hero />
+      <Hero {...HeroProps} />
       <Members />
       <Footer />
     </div>
